4e2ee789ff74f8881595d44407b16febdc88111b
jjmschofield <jjmschofield@googlemail.com>
2016-08-06 12:46:15 +0100

    Adding nights out component scaffold

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
-import { provideRouter, RouterConfig } from '@angular/router';
+import {provideRouter, RouterConfig} from '@angular/router';
 
-import { HomeComponent } from './home';
-import { AboutComponent } from './about';
+import {HomeComponent} from './home';
+import {AboutComponent} from './about';
+import {NightsOutComponent} from './nights-out'
 
-export const routes: RouterConfig = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent}
+export const routes:RouterConfig = [
+  {path: '', component: NightsOutComponent},
+  {path: 'about', component: AboutComponent}
 ];
 
 export const APP_ROUTER_PROVIDERS = [
