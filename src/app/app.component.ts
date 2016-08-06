7a5c5d4c5f862c5536a2fe664e547b877c8491c0
jjmschofield <jjmschofield@googlemail.com>
2016-08-06 12:08:42 +0100

    Adding bootstrap and ng-botstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
-
+import {NGB_DIRECTIVES, NGB_PRECOMPILE} from '@ng-bootstrap/ng-bootstrap';
 import { ApiService } from './shared';
 
 import '../style/app.scss';
@@ -12,7 +12,8 @@ import '../style/app.scss';
 @Component({
   selector: 'my-app', // <my-app></my-app>
   providers: [ApiService],
-  directives: [...ROUTER_DIRECTIVES],
+  directives: [...ROUTER_DIRECTIVES, NGB_DIRECTIVES],
+  precompile: [ NGB_PRECOMPILE ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgbdTypeaheadBasic} from '../ui/typeahead-basic';
 
 @Component({
   selector: 'my-home',
   templateUrl: './home.component.html',
+  directives: [NgbdTypeaheadBasic],
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
