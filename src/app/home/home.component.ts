4da8c4842acd276f422dbcca2a0e61bdc2d73691
jjmschofield <jjmschofield@googlemail.com>
2016-08-06 12:34:48 +0100

    Removing references to bootstrap test from home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {NgbdTypeaheadBasic} from '../ui/typeahead-basic';
 
 @Component({
   selector: 'my-home',
   templateUrl: './home.component.html',
-  directives: [NgbdTypeaheadBasic],
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
