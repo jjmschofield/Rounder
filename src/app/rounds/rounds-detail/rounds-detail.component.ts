04247151ee147dac89b3c4e51a4e068b8c0a8d83
jjmschofield <jjmschofield@googlemail.com>
2016-08-07 13:05:54 +0100

    Updating rounds detail to use product select

--- src/app/rounds/rounds-detail/rounds-detail.component.ts
+++ src/app/rounds/rounds-detail/rounds-detail.component.ts
@@ -3,28 +3,48 @@ import { Router, ActivatedRoute } from '@angular/router';
 
 import { NightsOutService } from '../../nights-out/nights-out.service';
 import { RoundModel } from '../../shared/models/round.model';
+import { ProductSelectComponent } from '../../products/product-select';
+import { ProductModel } from '../../shared/models/product.model'
 
 @Component({
   selector: 'rounds-detail',
   templateUrl: './rounds-detail.component.html',
+  directives: [ProductSelectComponent],
   styleUrls: ['./rounds-detail.component.scss']
 })
 export class RoundsDetailComponent implements OnInit {
 
-  nightOutIdSub: any;
-  rounds: RoundModel[];
+  nightOutIdSub : any;
+  round : RoundModel;
 
-  constructor (private nightsOutService: NightsOutService,
-               private router: Router,
-               private route: ActivatedRoute) {
+  constructor (private nightsOutService : NightsOutService,
+               private router : Router,
+               private route : ActivatedRoute) {
   }
 
   ngOnInit () {
     this.nightOutIdSub = this.route.params.subscribe(params => {
-      let id = params['nightOutId'];
-      this.nightsOutService.setCurrentNightOutById(id);
-      this.rounds = this.nightsOutService.currentNightOut.rounds;
+      let nightOutId = params['nightOutId'];
+      let roundId = params['roundId'];
+      this.round = this.nightsOutService.getNightOutById(nightOutId).getRoundById(roundId);
     });
+  }
+
+  addProduct (product : ProductModel) {
+    this.round.addProduct(product);
+  }
+
+  decreaseQty (product : ProductModel) {
+    if (product.qty > 1) {
+      product.decreaseQty();
+    }
+    else {
+      this.round.products.forEach((roundProduct, index) => { // TODO - this should be abstracted further
+        if (product === roundProduct) {
+          this.round.removeProduct(index);
+        }
+      });
+    }
   }
 
 }
