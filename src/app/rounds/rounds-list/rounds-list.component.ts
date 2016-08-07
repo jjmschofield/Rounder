0c1aa99332bbea2f68996dd3596b93c6845d76bb
jjmschofield <jjmschofield@googlemail.com>
2016-08-07 11:05:49 +0100

    Updating rounds list to show rounds and to trigger a selection when one is clicked

--- src/app/rounds/rounds-list/rounds-list.component.ts
+++ src/app/rounds/rounds-list/rounds-list.component.ts
@@ -1,11 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {ROUTER_DIRECTIVES} from '@angular/router';
+
+import {Round} from '../../shared/models/round';
 
 @Component({
   selector: 'rounds-list',
   templateUrl: './rounds-list.component.html',
+  directives: [ROUTER_DIRECTIVES],
   styleUrls: ['./rounds-list.component.scss']
 })
 export class RoundsListComponent implements OnInit {
+
+  @Input() rounds : Round[];
+  @Output() onSelected = new EventEmitter<number>();
 
   constructor() {
     // Do stuff
@@ -13,6 +20,10 @@ export class RoundsListComponent implements OnInit {
 
   ngOnInit() {
 
+  }
+
+  select(id:number){
+    this.onSelected.emit(id);
   }
 
 }
