24c75815b89dd80ed7d970dd43aeea03d8ece984
jjmschofield <jjmschofield@googlemail.com>
2016-08-07 11:02:45 +0100

    Updating bars near by to list bars from service and emit event

--- src/app/bars/bars-near-by/bars-near-by.component.ts
+++ src/app/bars/bars-near-by/bars-near-by.component.ts
@@ -1,18 +1,27 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
+import {Bar} from '../../shared/models/bar';
+import {BarService} from '../bar.service';
 
 @Component({
   selector: 'bars-near-by',
   templateUrl: './bars-near-by.component.html',
+  providers: [BarService],
   styleUrls: ['./bars-near-by.component.scss']
 })
 export class BarsNearByComponent implements OnInit {
 
-  constructor() {
-    // Do stuff
+  @Output() onSelected = new EventEmitter<Bar>();
+
+  bars : Bar[] = [];
+
+  constructor(private barService:BarService) {
+    this.bars = this.barService.getBars();
   }
 
-  ngOnInit() {
-    console.log('Hello Nights Out');
+  ngOnInit() {}
+
+  select(bar:Bar){
+    this.onSelected.emit(bar);
   }
 
 }
