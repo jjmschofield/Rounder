25e81cebbb45d6f4c6b40baf12e04d7199786ba7
jjmschofield <jjmschofield@googlemail.com>
2016-08-07 11:06:43 +0100

    Updating nights out detail to show list of rounds

--- src/app/nights-out/nights-out-detail/nights-out-detail.component.ts
+++ src/app/nights-out/nights-out-detail/nights-out-detail.component.ts
@@ -1,6 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, ROUTER_DIRECTIVES} from '@angular/router';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Router, ActivatedRoute, ROUTER_DIRECTIVES} from '@angular/router';
+
+import {Round} from '../../shared/models/round';
 import {RoundsListComponent} from '../../rounds/rounds-list';
+import {NightsOutService} from '../../nights-out/nights-out.service';
 
 @Component({
   selector: 'nights-out-detail',
@@ -10,14 +13,38 @@ import {RoundsListComponent} from '../../rounds/rounds-list';
 })
 export class NightsOutDetailComponent implements OnInit {
 
-  id:string;
+  nightOutIdSub:any;
+  nightOutId: number;
+  rounds:Round[];
 
-  constructor(private route:ActivatedRoute) {}
-
-  ngOnInit() {
-    this.route.params
-      .map(params => params['nightOutId'])
-      .subscribe((id) => this.id = id);
+  constructor(private nightsOutService:NightsOutService,
+              private router:Router,
+              private route:ActivatedRoute) {
   }
 
+  ngOnInit() {
+    this.nightOutIdSub = this.route.params.subscribe(params => {
+      let id = params['nightOutId'];
+
+      let nightOut = this.nightsOutService.setCurrentNightOutById(id);
+
+      if(nightOut){
+        this.nightOutId = this.nightsOutService.currentNightOut.id;
+        this.rounds = this.nightsOutService.currentNightOut.rounds;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.nightOutIdSub.unsubscribe();
+  }
+
+  showPreviousRounds(){
+    return this.rounds.length > 0;
+  }
+
+  viewRound(roundId:number){
+    console.log('fuck you');
+    this.router.navigate(['/nights-out',this.nightOutId,'/rounds',roundId]);
+  }
 }
