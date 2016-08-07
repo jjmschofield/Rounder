74edf4e0140e3e836c7b849603c3f716298cd87f
jjmschofield <jjmschofield@googlemail.com>
2016-08-07 11:04:02 +0100

    Updaing rounds - create to handle bar selection

--- src/app/rounds/rounds-create/rounds-create.component.ts
+++ src/app/rounds/rounds-create/rounds-create.component.ts
@@ -1,21 +1,44 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
+
+import {Bar} from '../../shared/models/bar';
+import {NightOut} from '../../shared/models/nightOut';
+import {NightsOutService} from '../../nights-out/nights-out.service';
 import {BarsNearByComponent} from '../../bars/bars-near-by';
 import {BarsSearchComponent} from '../../bars/bars-search';
 
 @Component({
   selector: 'rounds-create',
   templateUrl: './rounds-create.component.html',
-  directives: [BarsNearByComponent,BarsSearchComponent],
+  directives: [BarsNearByComponent, BarsSearchComponent],
   styleUrls: ['./rounds-create.component.scss']
 })
 export class RoundsCreateComponent implements OnInit {
 
-  constructor() {
-    // Do stuff
+  nightOutIdSub: any;
+
+  constructor(private nightsOutService:NightsOutService,
+              private router:Router,
+              private route:ActivatedRoute) {
   }
 
   ngOnInit() {
-    console.log('Hello Bars Near By');
+    this.nightOutIdSub = this.route.params.subscribe(params => {
+      let id = params['nightOutId'];
+      this.nightsOutService.setCurrentNightOutById(id);
+    });
   }
+
+  ngOnDestroy(){
+    this.nightOutIdSub.unsubscribe();
+  }
+
+  selectBar(bar:Bar) {
+    let nightOutId: number = this.nightsOutService.currentNightOut.id;
+    let round = this.nightsOutService.getNightOutById(nightOutId).addRound(bar);
+    let link = ['/nights-out', nightOutId, '/rounds', round.id];
+    this.router.navigate(link);
+  }
+
 
 }
