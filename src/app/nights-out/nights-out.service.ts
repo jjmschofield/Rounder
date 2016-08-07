39780d36359f809746168eef3b4c5cedaadaca47
jjmschofield <jjmschofield@googlemail.com>
2016-08-07 10:58:45 +0100

    Adding nights out service

---
+++ src/app/nights-out/nights-out.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
+import {NightOut} from '../shared/models/nightOut';
+
+@Injectable()
+export class NightsOutService{
+  nightsOut : NightOut[] = [];
+  currentNightOut : NightOut;
+
+  constructor( private router:Router){
+  }
+
+  setCurrentNightOutById(id:number){ //TODO - this feels awkward here, maybe a nav service?
+
+    let nightOut : NightOut = this.getNightOutById(id);
+
+    if(nightOut){
+      this.currentNightOut = nightOut;
+      return this.currentNightOut;
+    }
+    else{
+      this.router.navigate(['/']);
+    }
+  }
+
+  createNightOut(){
+    let nightOut = new NightOut(
+      this.nightsOut.length,
+      Date.now()
+    );
+
+    this.nightsOut.push(nightOut);
+
+    return nightOut;
+  }
+
+  getNightOutById(id:number){
+
+    for(let nightOut of this.nightsOut){
+      if(nightOut.id == id){
+        return nightOut;
+      }
+    }
+
+    return null;
+  }
+
+}
