43044eccf439406cc9d5e6f1ae8399da97be8e4a
jjmschofield <jjmschofield@googlemail.com>
2016-08-09 10:02:35 +0100

    Adding nav bar to nights out overview

--- src/app/nights-out/nights-out-overview/nights-out-overview.component.ts
+++ src/app/nights-out/nights-out-overview/nights-out-overview.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+
+import { NavBarComponent } from '../../nav-bar';
+
 import { NightsOutListComponent } from '../nights-out-list';
 import { NightsOutService } from '../nights-out.service';
 import { NightOutModel } from '../../shared/models/night-out.model';
 
 @Component({
   selector: 'nights-out',
-  directives: [NightsOutListComponent],
+  directives: [NightsOutListComponent, NavBarComponent],
   templateUrl: './nights-out-overview.component.html',
   styleUrls: ['./nights-out-overview.component.scss']
 })
