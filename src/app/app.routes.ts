8321d5d494ad1fb2be88aa7d2401d8a167f49473
jjmschofield <jjmschofield@googlemail.com>
2016-08-14 13:41:30 +0100

    Refactoring nights-out-overview component to simply be nights-out component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { provideRouter, RouterConfig } from '@angular/router';
-import { NightsOutOverviewComponent } from './nights-out/nights-out-overview';
+import { NightsOutComponent } from './nights-out';
 import { NightsOutDetailComponent } from './nights-out/nights-out-detail';
 import { RoundsCreateComponent } from './rounds/rounds-create';
 import { RoundsDetailComponent } from './rounds/rounds-detail';
 
 export const routes : RouterConfig = [
-  {path: '', component: NightsOutOverviewComponent},
-  {path: 'nights-out', component: NightsOutOverviewComponent},
+  {path: '', component: NightsOutComponent},
+  {path: 'nights-out', component: NightsOutComponent},
   {path: 'nights-out/:nightOutId', component: NightsOutDetailComponent},
   {path: 'nights-out/:nightOutId/rounds/create', component: RoundsCreateComponent},
   {path: 'nights-out/:nightOutId/rounds/:roundId', component: RoundsDetailComponent}
